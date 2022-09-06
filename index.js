6aab241a58294b2e96350b202690b53fe4eaf56d
Daniel Mason <danmasson0@gmail.com>
2022-09-06 10:44:53 -0400

    created a js file to house the quiz logic seperately from the main js file.

--- index.js
+++ index.js
@@ -6,36 +6,3 @@ const result = document.querySelector(".result");
 toolTips.forEach((t) => {
   new bootstrap.Tooltip(t);
 });
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  let score = 0;
-  const userAnswers = [
-    form.q1.value,
-    form.q2.value,
-    form.q3.value,
-    form.q4.value,
-  ];
-  // Check answer
-  userAnswers.forEach((answer, index) => {
-    if (answer === correctAnswers[index]) {
-      score += 25;
-    }
-  });
-  // show result
-  scrollTo(0, 0);
-
-  result.classList.remove("d-none");
-  let userResult = 0;
-  const timer = setInterval(() => {
-    {
-      result.querySelector("span").textContent = `${userResult}%`;
-      if (userResult === score) {
-        clearInterval(timer);
-      } else {
-        userResult++;
-      }
-    }
-  }, 10);
-});
