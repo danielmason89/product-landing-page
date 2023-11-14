04544ab82e0721c36571546d0063a189faf6fbf8
Daniel Mason <danmasson0@gmail.com>
2023-11-14 18:19:06 -0500

    made a small change to the time and date

--- clock.js
+++ clock.js
@@ -5,8 +5,8 @@ const tick = () => {
   const now = new Date();
 
   const html = `
-  <span>${dateFns.format(now, "MMMM dddd Do / YYYY")}</span>
-  <span>${dateFns.format(now, "h : mm : ss a")}</span>
+  <span>${dateFns.format(now, "MMMM dddd Do / YY")}</span>
+  <span>${dateFns.format(now, "h:mm a")}</span>
   `;
 
   clock.innerHTML = html;
