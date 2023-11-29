233485cefaf6038fb8d74c3a7f29d5e5bdce2f82
Daniel Mason <danmasson0@gmail.com>
2023-11-29 00:06:16 -0500

    built out webpack config more and refactored some styles for login/signup pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,44 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
+let production = process.env.NODE_ENV === "production";
+
+let config = {
   mode: "development",
+  devServer: {
+    liveReload: true,
+    watchFiles: ["src/**/* css/**/*"],
+    static: "./dist",
+  },
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env"] },
+        },
+      },
+      {
         test: /\.css$/,
+        exclude: /node-modules/,
         use: ["style-loader", "css-loader"],
       },
     ],
   },
-  watch: true,
+  plugins: [],
 };
+
+if (production) {
+  (config.mode = "production"), (config.devtool = "eval");
+}
+
+module.exports = config;
