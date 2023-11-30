09b4e63fd20ad46dc39afb9fbc1d389038450ec4
Daniel Mason <danmasson0@gmail.com>
2023-11-30 12:23:29 -0500

    further implement webpack effiencies to improve build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugIn = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 let production = process.env.NODE_ENV === "production";
@@ -10,15 +11,21 @@ let config = {
     liveReload: true,
     watchFiles: ["src/**/* css/**/*"],
     static: "./dist",
+    open: true,
+    hot: true,
   },
-  entry: "./src/index.js",
+  entry: {
+    main: path.resolve(__dirname, "src/index.js"),
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    clean: true,
   },
   devtool: "inline-source-map",
   module: {
     rules: [
+      // Javascript
       {
         test: /\.js$/,
         exclude: /node-modules/,
@@ -27,14 +34,59 @@ let config = {
           options: { presets: ["@babel/preset-env"] },
         },
       },
+      // CSS
       {
         test: /\.css$/,
         exclude: /node-modules/,
         use: ["style-loader", "css-loader"],
       },
+      // Images/Assets
+      {
+        test: /\.(svg|ico|png|webp|jpg|gif|jpeg)$/,
+        type: "assets/resource",
+      },
     ],
   },
-  plugins: [],
+  plugins: [
+    new CopyPlugIn({
+      patterns: [{ from: "src/assets", to: "assets" }],
+    }),
+    new HtmlWebpackPlugin({
+      title: "Home",
+      template: path.resolve(__dirname, "src/index.html"),
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Blog",
+      template: path.resolve(__dirname, "src/blog.html"),
+      filename: "blog.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Quiz",
+      template: path.resolve(__dirname, "src/quiz.html"),
+      filename: "quiz.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Login",
+      template: path.resolve(__dirname, "src/login.html"),
+      filename: "login.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Signup",
+      template: path.resolve(__dirname, "src/signup.html"),
+      filename: "signup.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Shop",
+      template: path.resolve(__dirname, "src/shop.html"),
+      filename: "shop.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Checkout",
+      template: path.resolve(__dirname, "src/checkout.html"),
+      filename: "checkout.html",
+    }),
+  ],
 };
 
 if (production) {
